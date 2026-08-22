ae533216de6c39ac8a6adcc5125b25c99fbd5a80
glabular <glu.max06@gmail.com>
2026-08-22 17:42:37 +0200

    Wire WebView2 and add static web UI folder

--- AnyChat.NET.Desktop/MainWindow.xaml.cs
+++ AnyChat.NET.Desktop/MainWindow.xaml.cs
@@ -1,23 +1,21 @@
-﻿using System.Text;
+﻿using System.IO;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 
 namespace AnyChat.NET.Desktop;
 
-/// <summary>
-/// Interaction logic for MainWindow.xaml
-/// </summary>
 public partial class MainWindow : Window
 {
     public MainWindow()
     {
         InitializeComponent();
+        Loaded += OnLoaded;
+    }
+
+    private async void OnLoaded(object sender, RoutedEventArgs e)
+    {
+        await WebView.EnsureCoreWebView2Async();
+
+        var htmlPath = Path.Combine(AppContext.BaseDirectory, "www", "index.html");
+        WebView.Source = new Uri(htmlPath);
     }
 }
