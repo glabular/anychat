8ed2305d5c47151a26ca50f4800b4b62962c08c0
glabular <glu.max06@gmail.com>
2026-08-26 00:06:57 +0200

    Expose chat messages list on the API

--- AnyChat.NET.Api/Controllers/ChatsController.cs
+++ AnyChat.NET.Api/Controllers/ChatsController.cs
@@ -15,4 +15,24 @@ public class ChatsController(AnytypeClient client) : ControllerBase
         var response = await client.Chats.ListAsync(spaceId);
         return Ok(response.Chats);
     }
+
+    [HttpGet("{chatId}/messages")]
+    public async Task<IActionResult> ListMessages(
+        string spaceId,
+        string chatId,
+        [FromQuery] int limit = 1)
+    {
+        if (limit < 1)
+        {
+            limit = 1;
+        }
+        else if (limit > 1000)
+        {
+            limit = 1000;
+        }
+
+        var response = await client.Chats.ListMessagesAsync(spaceId, chatId, limit: limit);
+        
+        return Ok(response.Messages ?? []);
+    }
 }
